ae1598c7816d66dad716a4f2bcc92be50df918e3
Brian Murphy <bmurphy2019@gmail.com>
2023-04-13 17:12:37 +0100

    Css improvements

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,139 @@
 <template>
   <body>
     <div id="main-content">
-        <div id = "calculator">
-                <label for="initialCapital"> Starting Capital:</label>
-                <input v-model="initialCapital" type="number" id="initialCapital" name="initialCapital"> <br>
+      <div id="calculator"
+        :class="{ calcuatorDesignBStart: this.isInExperiment && this.resultsHiden && !this.slideAnimation, calcuatorDesignBEnd: this.isInExperiment && !this.resultsHiden, calcuatorDesignA: !this.isInExperiment, divSlide: this.slideAnimation }">
 
-                <label for="optionalContributionValue"> Additional Contribution:</label>
-                <input v-model="optionalContributionValue" type="number" id="optionalContributionValue" name="optionalContributionValue"> <br>
-
-                <label for="optionalContributionFequency"> Additional contribution every </label>
-                <input v-model="optionalContributionFequency" type="number" id="optionalContributionFequency" name="optionalContributionFequency"> 
-                <select v-model="optionalContributionPeriod" id="optionalContributionPeriod" name="optionalContributionPeriod">
-                    <option value="month">Months</option>
-                    <option value="year">Years</option>
-                  </select> <br>
-                
-                <label for="compoundFrequency"> Intrest rate: </label>
-                <input v-model="compoundFrequency" type="number" id="compoundFrequency" name="compoundFrequency"> 
-                <select v-model="compoundFrequencyPeriod" id="compoundFrequencyPeriod" name="compoundFrequencyPeriod">
-                    <option value="month">Monthly</option>
-                    <option value="year">Yearly</option>
-                </select> <br>
-
-                <label for="optinalFeesValue"> Optional tax / fees : </label>
-                <input v-model="optinalFeesValue" type="number" id="optinalFeesValue" name="optinalFeesValue"> 
-                <select v-model="optinalFeesPeriod" id="optinalFeesPeriod" name="optinalFeesPeriod">
-                    <option value="month">Monthly</option>
-                    <option value="year">Yearly</option>
-                </select> <br>
-
-                <label for="investmentPeriodValue"> Invested for: </label>
-                <input v-model="investmentPeriodValue" type="number" id="investmentPeriodValue" name="investmentPeriodValue"> 
-                <select v-model="investmentPeriod" id="investmentPeriod" name="investmentPeriod">
-                    <option value="month">Months</option>
-                    <option value="year">Years</option>
-                </select> <br>
-                <button @click="caluclate"> Submit</button>
-
-        </div>
-        <div id ="results-body" >
-          <div id="app">
-            <Chart v-bind:savings="savings" />
+        <div class="table-row">
+          <div class="table-cell">
+            <label for="initialCapital"> Starting Capital: </label>
           </div>
-            <div id="text-results">
-            <p> Total after investment €{{ compoundJourney }} </p>
-            <p> Total gained €{{ compoundGains }} </p>
-            </div>
+          <div class="table-cell">
+            <span class="input-euro left">
+              <input v-model="initialCapital" type="number" id="initialCapital" name="initialCapital"> <br>
+            </span>
+          </div>
+          <div class="table-cell" v-if="this.resultsHiden && !this.slideAnimation">
+            <p>
+              &lt;- How much money are you starting with?
+            </p>
+          </div>
         </div>
+
+        <div class="table-row">
+          <div class="table-cell">
+            <label for="optionalContributionValue"> Additional Contribution:</label>
+          </div>
+          <div class="table-cell">
+            <span class="input-euro left">
+              <input v-model="optionalContributionValue" type="number" id="optionalContributionValue"
+                name="optionalContributionValue"> <br>
+            </span>
+          </div>
+          <div class="table-cell" v-if="this.resultsHiden && !this.slideAnimation">
+            <p>
+              &lt;- How much money, if any, are you adding periodically?
+            </p>
+          </div>
+        </div>
+
+        <div class="table-row">
+          <div class="table-cell">
+            <label for="optionalContributionFequency"> Additional contribution every </label>
+          </div>
+          <div class="table-cell">
+            <input v-model="optionalContributionFequency" type="number" id="optionalContributionFequency"
+              name="optionalContributionFequency">
+            <select v-model="optionalContributionPeriod" id="optionalContributionPeriod"
+              name="optionalContributionPeriod">
+              <option value="month">Months</option>
+              <option value="year">Years</option>
+            </select> <br>
+          </div>
+          <div class="table-cell" v-if="this.resultsHiden && !this.slideAnimation">
+            <p>
+              &lt;- How often are you adding money?
+            </p>
+          </div>
+        </div>
+
+        <div class="table-row">
+          <div class="table-cell">
+            <label for="compoundFrequency"> Expected intrest rate: </label>
+          </div>
+          <div class="table-cell">
+            <span class="input-percent right">
+              <input v-model="compoundFrequency" type="number" id="compoundFrequency" name="compoundFrequency">
+            </span>
+            <select v-model="compoundFrequencyPeriod" id="compoundFrequencyPeriod" name="compoundFrequencyPeriod">
+              <option value="month">Monthly</option>
+              <option value="year">Yearly</option>
+            </select> <br>
+          </div>
+          <div class="table-cell" v-if="this.resultsHiden && !this.slideAnimation">
+            <p>
+              &lt;- what percent gains do you expect and how often?
+            </p>
+          </div>
+        </div>
+
+        <div class="table-row">
+          <div class="table-cell">
+            <label for="optinalFeesValue"> Optional tax / fees : </label>
+          </div>
+          <div class="table-cell">
+            <span class="input-euro left">
+              <input v-model="optinalFeesValue" type="number" id="optinalFeesValue" name="optinalFeesValue">
+            </span>
+            <select v-model="optinalFeesPeriod" id="optinalFeesPeriod" name="optinalFeesPeriod">
+              <option value="month">Monthly</option>
+              <option value="year">Yearly</option>
+            </select> <br>
+          </div>
+          <div class="table-cell" v-if="this.resultsHiden && !this.slideAnimation">
+            <p>
+              &lt;- How much are you being charged per month / year ?
+            </p>
+          </div>
+        </div>
+
+        <div class="table-row">
+          <div class="table-cell">
+            <label for="investmentPeriodValue"> Invested for: </label>
+          </div>
+          <div class="table-cell">
+            <input v-model="investmentPeriodValue" type="number" id="investmentPeriodValue" name="investmentPeriodValue">
+            <select v-model="investmentPeriod" id="investmentPeriod" name="investmentPeriod">
+              <option value="month">Months</option>
+              <option value="year">Years</option>
+            </select> <br>
+          </div>
+          <div class="table-cell" v-if="this.resultsHiden && !this.slideAnimation">
+            <p>
+              &lt;- How long are you investing for?
+            </p>
+          </div>
+        </div>
+        <button @click="caluclate"> Submit</button>
+
+      </div>
+      <div v-if="!this.resultsHiden" id="results-body" :class="{ fadeIn: this.divFade }">
+        <div id="app">
+          <Chart v-bind:savings="savings" />
+        </div>
+        <div id="text-results">
+          <p> Total after investment €{{ calculatorResult }} </p>
+          <p> Total gained €{{ compoundGains }} </p>
+        </div>
+      </div>
     </div>
-</body>
+  </body>
 </template>
 
 <script>
 import Chart from './components/Compound-Chart.vue'
-import { getAnalytics, logEvent } from "firebase/analytics";
+// import { logEvent } from "firebase/analytics";
+// import { analytics } from "/public/index.html"
 
 export default {
   name: 'App',
@@ -62,25 +142,28 @@ export default {
   },
   data() {
     return {
-      analytics: getAnalytics(),
-      initialCapital: 0,
-      optionalContributionValue: 0,
-      optionalContributionFequency: 1,
+      divFade: false,
+      isInExperiment: true,
+      resultsHiden: true,
+      slideAnimation: false,
+      initialCapital: '',
+      optionalContributionValue: '',
+      optionalContributionFequency: '',
       optionalContributionPeriod: "year",
-      compoundFrequency: 0,
+      compoundFrequency: '',
       compoundFrequencyPeriod: "year",
-      optinalFeesValue: 0,
+      optinalFeesValue: '',
       optinalFeesPeriod: "year",
       investmentPeriodValue: 0,
       investmentPeriod: "year",
       compoundJourney: [],
-      calculatorResult:0,
-      compoundGains:0,
+      calculatorResult: 0,
+      compoundGains: 0,
       savings: {
         compound: [],
         contributions: [],
         labels: [],
-    }
+      }
     }
   },
   computed: {
@@ -102,7 +185,7 @@ export default {
     },
     feesEveryXMonths: function () {
       if (this.optinalFeesPeriod == "year") {
-        return  12
+        return 12
       }
       else {
         return 1
@@ -120,38 +203,41 @@ export default {
       else return 0
     },
   },
-  methods:{
-    caluclate(){
-      logEvent(this.analytics, 'notification_received');
+  methods: {
+    caluclate() {
+      if (this.isInExperiment && this.resultsHiden) {
+        this.animateDiv()
+      }
+      // logEvent(analytics, 'notification_received');
       this.compoundJourney = []
       let currentMoney = this.initialCapital
       let moneyInvested = [this.initialCapital]
       let chartLabels = []
-      if(currentMoney != 0 || currentMoney !=""){
+      if (currentMoney != 0 || currentMoney != "") {
         // eslint-disable-next-line
         this.compoundJourney.push(Math.round((currentMoney + Number.EPSILON) * 100) / 100)
       }
-      if(this.investmentPeriod =="year"){
+      if (this.investmentPeriod == "year") {
         chartLabels.push("Year 0")
-      }else{
+      } else {
         chartLabels.push("Month 0")
       }
       for (let i = 1; i <= this.investedTimeMonths; i++) {
-        currentMoney = currentMoney  * (1 + (this.monthlyIntrestRate / 100))
+        currentMoney = currentMoney * (1 + (this.monthlyIntrestRate / 100))
 
-        if(this.investmentPeriod=="year" && i%12 == 0){
-          chartLabels.push("Year "+i/12)
-        }else if(this.investmentPeriod =="month"){
-          chartLabels.push("Month "+i)
+        if (this.investmentPeriod == "year" && i % 12 == 0) {
+          chartLabels.push("Year " + i / 12)
+        } else if (this.investmentPeriod == "month") {
+          chartLabels.push("Month " + i)
         }
-        if(i%this.contributeEveryXMonths ==0 ){
-          currentMoney = currentMoney+ this.optionalContributionValue
-          moneyInvested.push(moneyInvested[moneyInvested.length-1]+ this.optionalContributionValue)
-        }else{
-          moneyInvested.push(moneyInvested[moneyInvested.length-1])
+        if (i % this.contributeEveryXMonths == 0) {
+          currentMoney = currentMoney + this.optionalContributionValue
+          moneyInvested.push(moneyInvested[moneyInvested.length - 1] + this.optionalContributionValue)
+        } else {
+          moneyInvested.push(moneyInvested[moneyInvested.length - 1])
         }
-        if(i%this.feesEveryXMonths ==0 ){
-          currentMoney = currentMoney- this.optinalFeesValue
+        if (i % this.feesEveryXMonths == 0) {
+          currentMoney = currentMoney - this.optinalFeesValue
         }
         this.compoundJourney.push(Math.round((currentMoney + Number.EPSILON) * 100) / 100)
       }
@@ -159,21 +245,29 @@ export default {
       let gains = this.calculatorResult - this.initialCapital
       this.compoundGains = Math.round((gains + Number.EPSILON) * 100) / 100
 
-      if(this.investmentPeriod=="month"){
+      if (this.investmentPeriod == "month") {
         this.savings.compound = this.compoundJourney
-        this.savings.contributions= moneyInvested
-      }else if(this.investmentPeriod == "year"){
-        let compoundGrowth=[]
-        let contributionGrowth=[]
-        for (let i = 0; i < this.compoundJourney.length; i=i+12) {
+        this.savings.contributions = moneyInvested
+      } else if (this.investmentPeriod == "year") {
+        let compoundGrowth = []
+        let contributionGrowth = []
+        for (let i = 0; i < this.compoundJourney.length; i = i + 12) {
           compoundGrowth.push(this.compoundJourney[i]);
           contributionGrowth.push(moneyInvested[i])
         }
         this.savings.compound = compoundGrowth
-        this.savings.contributions= contributionGrowth
+        this.savings.contributions = contributionGrowth
       }
 
       this.savings.labels = chartLabels
+    },
+    animateDiv() {
+      this.slideAnimation = true;
+      setTimeout(() => {
+        this.slideAnimation = false
+        this.resultsHiden = false
+        this.divFade = true
+      }, 1500)
     }
   }
 }
